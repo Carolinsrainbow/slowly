737b01c4a5a7fcc7b72fcf0a3b5f14a8de9dbcfe
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-02 21:57:00 -0400

    Stringfy

---
+++ stringfy.js
@@ -0,0 +1,15 @@
+const frutas = [
+  {
+    nombre: "manzana",
+    precio: 100,
+    existencia: 1,
+  },
+  {
+    nombre: "pera",
+    precio: 100,
+    existencia: 1,
+  },
+];
+
+const stringfyFrutas = JSON.stringify(frutas)
+console.log(stringfyFrutas)
